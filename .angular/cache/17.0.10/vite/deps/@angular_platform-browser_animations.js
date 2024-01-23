ce35fcb1d7b57783ed19b955939376cfe633f547
Riajul Kabir <riajul.kabir@welldev.io>
2024-01-23 13:38:03 +0600

    - configs_1

---
+++ .angular/cache/17.0.10/vite/deps/@angular_platform-browser_animations.js
@@ -0,0 +1,23 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-6NMVWBSP.js";
+import "./chunk-THFGBR4U.js";
+import "./chunk-PY3VLUSX.js";
+import "./chunk-7QCV2LIA.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-SWX36DQH.js";
+import "./chunk-V4QF72PL.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
