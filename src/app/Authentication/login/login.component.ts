a75b1d40eeb1e2a2a99259c9989bddf142287f6e
Riajul Kabir <riajul.kabir@welldev.io>
2024-01-23 13:41:16 +0600

    - authentication and authorization

---
+++ src/app/Authentication/login/login.component.ts
@@ -0,0 +1,45 @@
+import {Component} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Router, RouterLink, RouterLinkActive} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
+import {NgIf} from "@angular/common";
+
+@Component({
+    selector: 'app-login',
+    standalone: true,
+    imports: [
+        FormsModule,
+        RouterLink,
+        RouterLinkActive,
+        NgIf,
+        ReactiveFormsModule,
+    ],
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.css'
+})
+export class LoginComponent {
+    loginForm!: FormGroup;
+
+    constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+        this.loginForm = this.fb.group({
+            username: new FormControl('', [Validators.required]),
+            password: new FormControl('', [Validators.required]),
+        });
+    }
+
+    onSubmit() {
+        if (this.loginForm.valid) {
+            this.authService.onLogin(this.loginForm.value).subscribe({
+                next: () => {
+                    this.router.navigate(['home']);
+                }
+            });
+        } else {
+            this.loginForm.markAllAsTouched();
+        }
+    }
+
+    goToRegistration() {
+        this.router.navigate(['register'])
+    }
+}
---
+++ src/app/Interceptor/auth.interceptor.ts
@@ -0,0 +1,35 @@
+import {HttpErrorResponse, HttpInterceptorFn} from '@angular/common/http';
+import {constants} from "../constants/constants";
+import {Router} from "@angular/router";
+import {TokenService} from "../services/token.service";
+import {inject} from "@angular/core";
+import {catchError, throwError} from "rxjs";
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const tokenService = inject(TokenService);
+  const router = inject(Router);
+
+  tokenService.isAuthentication.subscribe({
+    next: (value) => {
+      if (value) {
+        req = req.clone({
+          setHeaders: {
+            Authorization: `Bearer ${tokenService.getToken()}`,
+          },
+        });
+      }
+    },
+  });
+
+  return next(req).pipe(
+      catchError((e: HttpErrorResponse) => {
+        if (e.status === 401) {
+          tokenService.removeToken();
+          router.navigate(['']);
+        }
+        const error = e.error?.error?.message || e.statusText;
+        return throwError(() => error);
+      })
+  );
+};
